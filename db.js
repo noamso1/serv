50b8218d4e1351203977781fd4c5289bd5784cce
noamso <noamso@moovex.net>
2021-08-10 09:46:08 +0000

    .

--- db.js
+++ db.js
@@ -30,14 +30,25 @@ async function dbDo(q, user) {
   let r = {};
   let vres = await validate(q, user); if (vres.error) return vres;
 
-  if (q.act == 'find') {
+  if (q.act == 'find' && q.count) {
+    if (q.count) r.count = await global.db.collection(q.col).find(q.query,{}).count()
+  }
+  if (q.act == 'find' && q.count != 2) {
     if (q.count) r.count = await global.db.collection(q.col).find(q.query,{}).count();
     let skip = q.skip; if (!skip) skip = 0;
     let limit = q.limit; if (!limit) limit = 999999;
     let sort = q.sort; if (!sort) sort = {};
     let project = q.project; if (!project) project = {};
+    if (q.sums) {
+      r.sums = []
+      let ag = [ { $match: q.query }, { $group: { _id: '1' } } ]
+      for ( let i = 0; i < q.sums.length; i++ ) ag[1].$group['sum' + i] = { $sum: '$' + q.sums[i] }
+      let aa = await db.collection(q.col).aggregate(ag).toArray()
+      for ( let i = 0; i < q.sums.length; i++ ) r.sums.push( aa[0] ? aa[0]['sum' + i] : 0 )        
+    }
     r.data = await global.db.collection(q.col).find(q.query).skip(skip).project(project).limit(limit).sort(sort).toArray()
   }
+
   else if (q.act == 'insert' && q.data.length) {
     r = await global.db.collection(q.col).insertMany(q.data)
   }
