cbde469d61688c190ecc106680936fcd8e899dee
noamso <noamso@moovex.net>
2021-10-08 21:39:22 +0300

    .

--- sched.js
+++ sched.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 "use strict"; //
 const func = require("./func.js")
-let sched_user = {"email": "SCHED"}
 let counter = 0; function every(z) { return counter / z == ( counter / z ).toFixed() }
 
 if ( !process.env.NODE_APP_INSTANCE || process.env.NODE_APP_INSTANCE == 0 ) setInterval(sched, 60000) // run only on first instance
