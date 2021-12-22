38467b071e6f1518177fb71a55aafe67c65f0238
noamso <noamso@moovex.net>
2021-12-22 13:31:20 +0200

    .

--- email.js
+++ email.js
@@ -7,6 +7,7 @@ async function send(msg) { //from,to,subject,html
   if ( global.env.smtpService ) {
     // 1. should enable: myaccount.google.com / security / less secure app access
     // 2. then you have to change you password in google before this works.
+    // 3. optional: enable 2-Step Verification, goto App Passwords, generate pass and use this one, then disable step 1 (less secure app access).
     // note: "to" supports one string with commas for multiple recipients
     const trans = require('nodemailer').createTransport({ service: global.env.smtpService, auth: { user: global.env.smtpUser, pass: global.env.smtpPass } })
     trans.verify().then().catch(console.error);
