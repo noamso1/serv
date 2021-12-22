235b85d5b25d2d40fd3b8f1cae932f72cde9943b
noamso <noamso@moovex.net>
2021-12-22 13:26:23 +0200

    .

--- email.js
+++ email.js
@@ -1,11 +1,24 @@
 'use strict'
-const sgMail  = require ('@sendgrid/mail')
-sgMail.setApiKey(global.env.sgKey)
-
+let sendGrid, sendGridConnected
 async function send(msg) { //from,to,subject,html
+
   console.log(msg)
-  if (global.dbName == 'serv') return // local
-  return sgMail.send(msg)
+
+  if ( global.env.smtpService ) {
+    // 1. should enable: myaccount.google.com / security / less secure app access
+    // 2. then you have to change you password in google before this works.
+    // note: "to" supports one string with commas for multiple recipients
+    const trans = require('nodemailer').createTransport({ service: global.env.smtpService, auth: { user: global.env.smtpUser, pass: global.env.smtpPass } })
+    trans.verify().then().catch(console.error);
+    let m = msg; delete m.from
+    let r = await trans.sendMail(m)
+    return r
+
+  } else if ( global.env.sendGridKey ) {
+    if ( !sendGridConnected ) sendGrid = require('@sendgrid/mail').setApiKey(global.env.sendGridKey)
+    let r = await sendGrid.send(msg)
+    return r
+  }
 }
 
 module.exports = { send }
